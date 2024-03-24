fb32d7514434838df20d3135b4f6b6e91533eebe
Erwan <poire.erwan2005@gmail.com>
2024-03-24 19:18:28 +0100

    Positionnement de la Barre de Navigation : J'ai modifié la barre de navigation pour qu'elle reste fixée en haut de l'écran, mais il semble y avoir un problème avec son alignement qui n'est pas parfaitement en haut. Je vais devoir enquêter sur la raison pour laquelle elle ne se positionne pas correctement.
    Fonctionnalité des Menus Déroulants : Bien que j'aie ajouté une fonctionnalité de base pour basculer les menus déroulants, il reste des améliorations à apporter. Actuellement, les menus déroulants pourraient ne pas fonctionner comme prévu. Je vais corriger la fonction des menus déroulants pour m'assurer qu'ils s'ouvrent et se ferment correctement.

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,31 +1,24 @@
-$(document).ready(function()
-{
-  $(document).ready(function() {
-    $('.dropdown-toggler').click(function() {
+"use strict";
+
+$(document).ready(function () {
+  $(document).ready(function () {
+    $('.dropdown-toggler').click(function () {
       var dropdownId = $(this).data('dropdown');
       $('#' + dropdownId).toggle();
     });
-  });  
-  $('#load-more-content').click(function()
-  {
-    
+  });
+  $('#load-more-content').click(function () {
     $('#more-content').toggleClass('shown');
-
     $('#load-more-content').hide();
 
-    if( $('#more-content').hasClass('shown') )
-    {
+    if ($('#more-content').hasClass('shown')) {
       $('#load-more-content').text('Hide content');
-      $('#more-content').fadeIn('slow', function()
-      {
+      $('#more-content').fadeIn('slow', function () {
         $('#load-more-content').fadeIn('slow');
       });
-    }
-    else
-    {
+    } else {
       $('#load-more-content').text('Load some content');
-      $('#more-content').fadeOut('slow', function()
-      {
+      $('#more-content').fadeOut('slow', function () {
         $('#load-more-content').fadeIn('slow');
       });
     }
